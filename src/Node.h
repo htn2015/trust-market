14b1669e2923702ce106e44fae3067e4132b0f1c
Andrei Savin <glork2006@gmail.com>
2015-09-19 14:26:35 -0400

    Structure

---
+++ src/Node.h
@@ -0,0 +1,10 @@
+#ifndef HTN2015_NODE_H
+#define HTN2015_NODE_H
+
+
+class Node {
+
+};
+
+
+#endif //HTN2015_NODE_H
---
+++ src/Trust.h
@@ -0,0 +1,14 @@
+#ifndef HTN2015_TRUST_H
+#define HTN2015_TRUST_H
+
+
+struct Trust {
+    int b,d,u;
+    Trust(int x, int y, int z) : b(x), d(y), u(z) {};
+
+    Trust recommendation(Trust AB, Trust BC);
+    Trust consensus(Trust Ap, Trust Bp);
+};
+
+
+#endif //HTN2015_TRUST_H
