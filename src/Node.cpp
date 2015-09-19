76f86940e4b8ca9e78b2896930ba1d37c3d8a8a1
Andrei Savin <glork2006@gmail.com>
2015-09-19 14:29:01 -0400

    Structure

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-=======
-//
-// Created by Andrei on 15-09-19.
-//
-
->>>>>>> 666deeff83547a9e6623ba3b1b78cf9471eaa6da
 #include "Node.h"
--- src/Node.h
+++ src/Node.h
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-=======
-//
-// Created by Andrei on 15-09-19.
-//
 
->>>>>>> 666deeff83547a9e6623ba3b1b78cf9471eaa6da
 #ifndef HTN2015_NODE_H
 #define HTN2015_NODE_H
 
