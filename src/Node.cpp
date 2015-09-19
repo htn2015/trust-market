6a45865f0defc3036753750360ddc1bbc059fea8
FrozenWinters <tkolomatski@hotmail.com>
2015-09-19 15:07:34 -0400

    upd8

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,1 +1,7 @@
 #include "Node.h"
+
+Graph::Graph(vector<Graph*> thought, int key) : key(key) {
+	for(vector<Graph*>::iterator i; i<thought.end(); i++){
+		keys.insert(i->keys.begin(), i->keys.end());
+	}
+}
--- src/Node.h
+++ src/Node.h
@@ -2,9 +2,16 @@
 #ifndef HTN2015_NODE_H
 #define HTN2015_NODE_H
 
+#include <vector>
+#include <map>
+#include <set>
 
-class Node {
 
+class Graph {
+	int key;
+	set<int> keys;
+	map<pair<int, int>, Trust> edges;
+	Node(vector <Graph*>, int key);
 };
 
 
