c6724e88acddf7e661f01ebbb634140999f309bf
FrozenWinters <tkolomatski@hotmail.com>
2015-09-19 16:48:05 -0400

    upd8

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,8 +1,18 @@
-#include "Node.h"
+#include "Graph.h"
+#include "Trust.h"
 
-Graph::Graph(vector<Graph*> thought, int key) : key(key) {
-	for(vector<Graph*>::iterator i; i<thought.end(); i++){
+Graph::Graph(vector<Graph*> thought, map<int, Trust> weights, int key) : key(key) {
+	for(vector<Graph*>::iterator i = thought.end(); i<thought.end(); i++){
 		keys.insert(i->keys.begin(), i->keys.end());
 	}
-	vector<>
+	for(set<int>::iterator i = keys.begin(); i<keys.end(); i++){
+		for(set<int>::iterator j = keys.begin(); j<keys.end(); j++){
+			Trust value(0,0,1);
+			for(vector<Graph*>::iterator p = thought.end(); p<thought.end(); p++){
+				map<pair<int, int>, Trust>::iterator z = p->edges.find(i->key, j->key);
+				if(z != p->edges.end())
+					value = consensus(value, recommendation(*weights.find(thought->key),*z));
+			}
+		}
+	}
 }
